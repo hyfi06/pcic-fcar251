2be414164347290e20805ffab84c41bc089f4ad3
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-08-29 09:31:13 -0600

    pi_montercalo con un master osioso
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- pi_montecarlo.m
+++ pi_montecarlo.m
@@ -1,6 +1,9 @@
-% parpool(4);
+p = gcp('nocreate'); % If no pool, do not create new one.
+if isempty(p)
+    parpool('local',4);
+end
 
-NUM_POINTS = 10000;
+NUM_POINTS = 100000;
 
 spmd
     pid = spmdIndex;
@@ -8,17 +11,19 @@ spmd
     NW = NP - 1;
     if pid == 1
         points = NW * NUM_POINTS;
-        seeds = rand(NW,1)*10000;
-        np_circ = 0;
-        spmdBroadcast(pid,NUM_POINTS);
+        seeds = randi(10000, NW, 1);
+        spmdBroadcast(pid, NUM_POINTS);
         for i=1:NW
             spmdSend(seeds(i,1),i+1);
         end
-        for i = 1:NW
-            [dato,source] = spmdReceive("any",1);
-            fprintf("Recibí %d de Worker %d\n",dato,source);
-            np_circ = np_circ + dato;
-        end
+        %np_circ=0;
+        %for i = 1:NW
+        %    [dato,source] = spmdReceive("any",1);
+        %    fprintf("Recibí %d de Worker %d\n",dato,source);
+        %    np_circ = np_circ + dato;
+        %end
+        np_circ = spmdPlus(0,1);
+        fprintf("Total=%d\n",np_circ);
         pi = 4 * np_circ / points;
         fprintf("Pi = %f\n",pi);
     else
@@ -29,14 +34,17 @@ spmd
             "Iniciando PID = %d con seeed = %d y puntos %d\n", ...
             pid,seed,num_points ...
         );
-        x_y = rand(num_points,2);
-        cir = 0;
+        circ = 0;
         for i = 1:num_points
-            if sqrt(x_y(i,1)^2 + x_y(i,2)^2) <= 1
-                cir = cir + 1;
+            point = rand(2,1);
+            d = sqrt(point(1,1)^2 + point(2,1)^2);  
+            if d <= 1
+                circ = circ + 1;
             end
         end
-        spmdSend(cir,1,1);
+        fprintf("Puntos locales: %d\n",circ);
+        spmdPlus(circ,1);
+        %spmdSend(circ,1,1);
     end
     fprintf("Fin, PID = %d\n",pid);
 end
