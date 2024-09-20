1fc2d18ef13d734342bf4e9f170f5a52b46d91df
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-09-19 21:53:52 -0600

    calse 19092024

---
+++ mandelbrot.m
@@ -0,0 +1,93 @@
+clear all;
+p = gcp('nocreate'); % If no pool, do not create new one.
+if isempty(p)
+    parpool(4); % MPI_INIT
+end
+
+%Protocolos
+TAG_DATOS = 1;
+TAG_RESULTADOS = 2;
+TAG_CONTRON = 3;
+
+FARMER_PID = 1;
+
+% Mandelbrot
+HIGHT = 480;
+WIDTH = 640;
+X_RANGE = struct();
+X_RANGE.min = -2;
+X_RANGE.max = 2;
+Y_RANGE = struct();
+Y_RANGE.min = -2;
+Y_RANGE.max = 2;
+
+spmd
+    pid = spmdIndex; % MPI_RANK
+    NP = spmdSize; % MPI_SIZE
+    NT = NP - 1;
+    if pid == 1 % Farmer
+        MAX_tareas = HIGHT;
+        tareas = 0;
+        ocupados = 0;
+        
+        % Inicialización
+        imagen = zeros(HIGHT,WIDTH);
+
+        % Primera etapa
+        for p = 1:NT
+            spmdSend(tareas+1, p+1, TAG_DATOS);
+            tareas = tareas+1;
+            ocupados = ocupados+1;
+        end
+
+        while (ocupados > 0)
+            [result, source, protocolo] = spmdReceive("any",TAG_RESULTADOS);
+            imagen(result.y) = result.val;
+            ocupados = ocupados - 1;
+            if tareas < MAX_tareas
+                % Segunda Etapa Trabajo sostenido
+                spmdSend(tareas+1, source, TAG_DATOS);
+                tareas = tareas+1;
+                ocupados = ocupados+1;
+            else
+                % Tercera Etapa, Terminacion
+                spmdSend(0,source,TAG_CONTRON);
+            end
+        end
+        save("image.m","imagen",'-fromstruct');
+    else % Worker  
+        [data, origen, protocolo] = spmdReceive(FARMER_PID);
+        while (protocolo == TAG_DATOS) 
+            fprintf("Procesando renglón %d\n", data);
+            res = get_range(data);
+            for col = 1:WITHD
+                res.val(1,col) = mandelbrot_pixel( ...
+                    X_RANGE.min + (col-1)*res.dx + res.y * 1i ...
+                );
+            end
+            spmdSend(res, FARMER_PID, TAG_RESULTADOS);
+            [data, origen, protocolo] = spmdReceive(FARMER_PID);
+        end
+    end
+    fprintf("Fin, pid=%d\n",pid)
+end
+
+
+function result = get_range(row)
+    result = struct();
+    result.row = row;
+    result.dx = (X_RANGE.max - X_RANGE.min)/(width-1);
+    result.dy = (Y_RANGE.max - Y_RANGE.min)/(HIGHT-1);
+    result.y = X_RANGE.max - (result.dy * (row - 1));
+    result.val = zeros(1,WIDTH);
+end
+
+function iter = mandelbrot_pixel(c)
+    MAX_ITER = 256;
+    z = 0+0i;
+    iter = 0;
+    while(abs(z) < 4 && iter < MAX_ITER)
+        z = z^2 + c;
+        iter = iter + 1;
+    end
+end
