451d09acbb0c6eea697135457453c99e8caf1102
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-11-21 18:47:17 -0600

    add malla
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

---
+++ malla.m
@@ -0,0 +1,49 @@
+% Malla carteciana
+
+% COL,ROW
+
+% 1,1 2,1 3,1
+% 1,2 2,2 3,2
+% 1,3 2,3 3,3
+
+clear all;
+
+p = gcp('nocreate'); % If no pool, do not create new one.
+
+if isempty(p)
+    c = parcluster;
+    c.NumWorkers = 9;
+    c.parpool(9); % MPI_INIT
+end
+
+NED = [3, 3]; % Número de elementos 
+
+spmd(9)
+    pid = spmdIndex;
+    NP = spmdSize;
+    ind = id2ind(pid,NED);
+    if prod(NED) ~= NP
+        exit
+    end
+    % dato local
+    local = pid;
+    ind_up = ind - [0 1];
+    ind_down = ind + [0 1];
+    ind_right = ind + [1 0];
+    ind_left = ind - [1 0];
+    
+    % Envio derecha
+    dato_right = spmdSendReceive(p_right,p_left,local);
+
+
+end
+
+function ind = id2ind(pid,NPD)
+    id = pid-1;
+    ind(2) = idivide(int32(id), int32(NPD(2))) +1;
+    ind(1) = mod(int32(id), int32(NPD(1))) + 1;
+end
+
+function id = ind2id(ind,NPD)
+    id = (int32(ind(2)) - 1) * int32(NPD(1))
+end
