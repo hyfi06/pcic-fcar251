748c2449fb0ad4ee68130ca8a2c67e0aba3bbc56
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-11-27 15:18:36 -0600

    malla
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- malla.m
+++ malla.m
@@ -16,34 +16,41 @@ if isempty(p)
     c.parpool(9); % MPI_INIT
 end
 
-NED = [3, 3]; % Número de elementos 
+NPD = [3, 3]; % Número de elementos 
 
 spmd(9)
     pid = spmdIndex; % MPI_Comm_rank 
     NP = spmdSize; % MPI_Comm_size
-    ind = id2ind(pid,NED);
-    if prod(NED) ~= NP
+    if prod(NPD) ~= NP
         exit
     end
+    ind = id2ind(pid,NPD);
+    display(ind);
     % dato local
     local = pid;
+    ind_left = ind - [1 0];
     ind_up = ind - [0 1];
     ind_down = ind + [0 1];
     ind_right = ind + [1 0];
-    ind_left = ind - [1 0];
+    
     
     % Envio derecha
-    dato_right = spmdSendReceive(p_right,p_left,local);
+    %dato_right = spmdSendReceive(p_right,p_left,local);
 
 
 end
 
-function ind = id2ind(pid,NPD)
+function ind = id2ind(pid, NPD)
     id = pid-1;
     ind(2) = idivide(int32(id), int32(NPD(2))) +1;
     ind(1) = mod(int32(id), int32(NPD(1))) + 1;
 end
 
-function id = ind2id(ind,NPD)
-    id = (int32(ind(2)) - 1) * int32(NPD(1))
+function id = ind2id(ind,NPD)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    id = (int32(ind(2)) - 1) * int32(NPD(1));
+end
+
+function result = nod(ind,incremento,NPD) 
+    result = ind + incremento;
+    result = [mod(result(0)-1,NPD(1)) mod((result(0)-1),NPDs(2))];
 end
