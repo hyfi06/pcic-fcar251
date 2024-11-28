98a88112ec6a9f3b5bbe3200efa63c0c2b479fcc
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-11-27 18:01:38 -0600

    malla snapshot
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- malla.m
+++ malla.m
@@ -16,7 +16,9 @@ if isempty(p)
     c.parpool(9); % MPI_INIT
 end
 
-NPD = [3, 3]; % Número de elementos 
+NPD = [3 3]; % Número de elementos COL ROW
+
+h = 1;
 
 spmd(9)
     pid = spmdIndex; % MPI_Comm_rank 
@@ -24,33 +26,49 @@ spmd(9)
     if prod(NPD) ~= NP
         exit
     end
-    ind = id2ind(pid,NPD);
-    display(ind);
+
+    ind = id2ind(pid, NPD);
     % dato local
     local = pid;
-    ind_left = ind - [1 0];
-    ind_up = ind - [0 1];
-    ind_down = ind + [0 1];
-    ind_right = ind + [1 0];
-    
-    
+
+    % Vecinos
+    cell_right = desplazamiento(ind, [1 0], NPD);
+    cell_left = desplazamiento(ind, [-1 0], NPD);
+    cell_up = desplazamiento(ind, [0 -1], NPD);
+    cell_down = desplazamiento(ind, [0 1], NPD);
+
+    p_right = ind2id(cell_right, NPD);
+    p_left = ind2id(cell_left, NPD);
+    p_up = ind2id(cell_up, NPD);
+    p_down = ind2id(cell_down, NPD);
+
     % Envio derecha
-    %dato_right = spmdSendReceive(p_right,p_left,local);
+    dato_right = spmdSendReceive(p_right, p_left, local);
+    
+    % Envio izquierda
+    dato_left = spmdSendReceive(p_left, p_right, local);
 
+    % Envio arriba
+    dato_up = spmdSendReceive(p_up, p_down, local);
 
+    % Envio arriba
+    dato_down = spmdSendReceive(p_down, p_up, local);
+
+    laplace = h * (dato_right + dato_left + dato_up + dato_down);
+    display(laplace)
 end
 
 function ind = id2ind(pid, NPD)
-    id = pid-1;
-    ind(2) = idivide(int32(id), int32(NPD(2))) +1;
-    ind(1) = mod(int32(id), int32(NPD(1))) + 1;
+    id = pid - 1;
+    ind(1) = mod(int32(id), int32(NPD(1))) + 1; % COL
+    ind(2) = idivide(int32(id), int32(NPD(1))) +1; % row
 end
 
-function id = ind2id(ind,NPD)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-    id = (int32(ind(2)) - 1) * int32(NPD(1));
+function id = ind2id(ind, NPD)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    id = (ind(2) - 1) * NPD(1) + ind(1);
 end
 
-function result = nod(ind,incremento,NPD) 
-    result = ind + incremento;
-    result = [mod(result(0)-1,NPD(1)) mod((result(0)-1),NPDs(2))];
+function result = desplazamiento(origen, delta, NPD)
+    result(1) = mod(int32(origen(1)-1) + int32(delta(1)), int32(NPD(1))) + 1;
+    result(2) = mod(int32(origen(2)-1) + int32(delta(2)), int32(NPD(2))) + 1;
 end
